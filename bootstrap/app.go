39ee9be5a80b08fd45127f6232228f0d704ba730
mehmetkaranlik <mhmtkrnlkk@gmail.com>
2024-12-07 21:16:06 +0300

    Add: reusable grpc implementation

--- bootstrap/app.go
+++ bootstrap/app.go
@@ -1,10 +1,14 @@
 package bootstrap
 
 type App struct {
-	HttpServer HttpServer
+	HttpServer   HttpServer
+	GRPCOperator *GRPCServerOperator
 }
 
 func (a *App) Run() {
 	go a.HttpServer.Start()
+	if a.GRPCOperator != nil {
+		go a.GRPCOperator.Start()
+	}
 	a.HttpServer.GracefulShutdown()
 }
---
+++ bootstrap/grpc.go
@@ -0,0 +1,52 @@
+package bootstrap
+
+import (
+	"github.com/gofiber/fiber/v2/log"
+	"google.golang.org/grpc"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+type GRPCServerOperator struct {
+	port       string
+	servers    []GRPCServer
+	opts       []grpc.ServerOption
+	grpcServer *grpc.Server
+}
+
+func NewGRPCServer(
+	port string,
+	servers []GRPCServer,
+	opts []grpc.ServerOption,
+) *GRPCServerOperator {
+	return &GRPCServerOperator{port: port, servers: servers, opts: opts}
+}
+
+func (g *GRPCServerOperator) Start() {
+	log.Infof("Starting GRPC server on port %s", g.port)
+	lis, err := net.Listen("tcp", ":"+g.port)
+	if err != nil {
+		log.Fatalf("Failed to start GRPC server on port: %v", err)
+	}
+	grpcServer := grpc.NewServer(g.opts...)
+	g.grpcServer = grpcServer
+	for _, server := range g.servers {
+		server.Register(grpcServer)
+	}
+	if err = grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
+
+func (g *GRPCServerOperator) GracefulShutdown() {
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, os.Interrupt, syscall.SIGTERM)
+	<-ch
+	g.grpcServer.GracefulStop()
+}
+
+type GRPCServer interface {
+	Register(server *grpc.Server)
+}
